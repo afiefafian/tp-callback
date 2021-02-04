ff478fe93071d5541c16dc71e61eb8c460569d55
afiefafian <mantafs123>
2021-02-04 16:42:02 +0700

    Add callback endpoint

--- src/main/java/com/github/afiefafian/tpcallback/controller/CallbackController.java
+++ src/main/java/com/github/afiefafian/tpcallback/controller/CallbackController.java
@@ -1,13 +1,40 @@
 package com.github.afiefafian.tpcallback.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/callback")
 public class CallbackController {
+    @PostMapping("order/notification")
+    public ResponseEntity<String> order_notification(@RequestBody String bodyData) {
+        System.out.println("Order Notification");
+        System.out.println(bodyData);
+        return new ResponseEntity<>(bodyData, HttpStatus.OK);
+    }
 
-    public String order_notification(data) {
+    @PostMapping("order/actions")
+    public ResponseEntity<String> order_actions(@RequestBody String bodyData) {
+        System.out.println("Order Actions");
+        System.out.println(bodyData);
+        return new ResponseEntity<>(bodyData, HttpStatus.OK);
+    }
 
+    @PostMapping("order/cancellation")
+    public ResponseEntity<String> order_cancellation(@RequestBody String bodyData) {
+        System.out.println("Order Cancellation");
+        System.out.println(bodyData);
+        return new ResponseEntity<>(bodyData, HttpStatus.OK);
+    }
+
+    @PostMapping("order/status")
+    public ResponseEntity<String> order_status(@RequestBody String bodyData) {
+        System.out.println("Order Status");
+        System.out.println(bodyData);
+        return new ResponseEntity<>(bodyData, HttpStatus.OK);
     }
 }
