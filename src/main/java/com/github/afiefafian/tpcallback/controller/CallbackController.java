452bb76afd4e9ebee15c310db5410beb8d342caf
afiefafian <mantafs123>
2021-02-22 10:02:50 +0700

    Add log request headers data

--- src/main/java/com/github/afiefafian/tpcallback/controller/CallbackController.java
+++ src/main/java/com/github/afiefafian/tpcallback/controller/CallbackController.java
@@ -2,38 +2,53 @@ package com.github.afiefafian.tpcallback.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
-@RequestMapping("/api/callback")
+@RequestMapping("/api/callback/order")
 public class CallbackController {
-    @PostMapping("order/notification")
-    public ResponseEntity<String> order_notification(@RequestBody String bodyData) {
+    @PostMapping("/notification")
+    public ResponseEntity<String> order_notification(
+            @RequestHeader Map<String, String> headers,
+            @RequestBody String bodyData
+    ) {
         System.out.println("Order Notification");
+        System.out.println(headers);
         System.out.println(bodyData);
         return new ResponseEntity<>(bodyData, HttpStatus.OK);
     }
 
-    @PostMapping("order/actions")
-    public ResponseEntity<String> order_actions(@RequestBody String bodyData) {
+    @PostMapping("/actions")
+    public ResponseEntity<String> order_actions(
+            @RequestHeader Map<String, String> headers,
+            @RequestBody String bodyData
+    ) {
         System.out.println("Order Actions");
+        System.out.println(headers);
         System.out.println(bodyData);
         return new ResponseEntity<>(bodyData, HttpStatus.OK);
     }
 
-    @PostMapping("order/cancellation")
-    public ResponseEntity<String> order_cancellation(@RequestBody String bodyData) {
+    @PostMapping("/cancellation")
+    public ResponseEntity<String> order_cancellation(
+            @RequestHeader Map<String, String> headers,
+            @RequestBody String bodyData
+    ) {
         System.out.println("Order Cancellation");
+        System.out.println(headers);
         System.out.println(bodyData);
         return new ResponseEntity<>(bodyData, HttpStatus.OK);
     }
 
-    @PostMapping("order/status")
-    public ResponseEntity<String> order_status(@RequestBody String bodyData) {
+    @PostMapping("/status")
+    public ResponseEntity<String> order_status(
+            @RequestHeader Map<String, String> headers,
+            @RequestBody String bodyData
+    ) {
         System.out.println("Order Status");
+        System.out.println(headers);
         System.out.println(bodyData);
         return new ResponseEntity<>(bodyData, HttpStatus.OK);
     }
